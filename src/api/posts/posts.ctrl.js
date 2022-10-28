6d02a78f3b83f2196741ba6d382c3abde67cb40d
Son <70love07@naver.com>
2022-10-28 11:25:27 +0900

    HTML을 필터링 하기위해 sanitize-html라이브러리 추가 또한 posts.ctrl 파일안의 list와 write, update 함수를 수정

--- src/api/posts/posts.ctrl.js
+++ src/api/posts/posts.ctrl.js
@@ -1,8 +1,33 @@
 import Post from '../../models/post';
 import mongoose from 'mongoose';
 import Joi from 'joi';
+import sanitizeHtml from 'sanitize-html';
 
 const { ObjectId } = mongoose.Types;
+
+const sanitizeOption = {
+    allowebTags: [
+        'h1',
+        'h2',
+        'b',
+        'i',
+        'u',
+        's',
+        'p',
+        'ul',
+        'ol',
+        'li',
+        'blockquote',
+        'a',
+        'img',
+    ],
+    allowebAttributes: {
+        a: ['href', 'name', 'target'],
+        img: ['src'],
+        li: ['class'],
+    },
+    allowebSchemes: ['data', 'http'],
+};
 
 export const getPostById = async (ctx, next) => {
     const { id } = ctx.params;
@@ -59,7 +84,7 @@ export const write = async ctx => {
     const { title, body, tags } = ctx.request.body;
     const post = new Post({
         title,
-        body,
+        body: sanitizeHtml(body, sanitizeOption),
         tags,
         user: ctx.state.user,
     });
@@ -69,6 +94,14 @@ export const write = async ctx => {
     } catch (e) {
         ctx.throw(500, e);
     }
+};
+
+// html을 없애고 내용이 너무 길면 200자로 제한하는 함수
+const removeHtmlAndShorten = body => {
+    const filtered = sanitizeHtml(body, {
+        allowedTags: [],
+    });
+    return filtered.length < 200 ? filtered : `${filtered.slice(0, 200)}...`;
 };
 
 /* 포스트 목록 조회
@@ -96,7 +129,7 @@ export const list = async ctx => {
         ctx.set('Last-Page', Math.ceil(postCount / 10));
         ctx.body = posts.map(post => ({
             ...post,
-            body: post.body.length < 200 ? post.body : `${post.body.slice(0, 200)}...`
+            body: removeHtmlAndShorten(post.body),
         }));
     } catch (e) {
         ctx.throw(500, e);
@@ -145,9 +178,15 @@ export const update = async ctx => {
         return;
     }
 
+    const nextData = { ...ctx.request.body }; // 객체를 복사하고
+    // body 값이 주어졌으면 HTML 필터링
+    if (nextData.body) {
+        nextData.body = sanitizeHtml(nextData.body, sanitizeOption);
+    }
     try {
-        const post = await Post.findByIdAndUpdate(id, ctx.request.body, {
-            new: true,
+        const post = await Post.findByIdAndUpdate(id, nextData, {
+            new: true, // 이 값을 설정하면 업데이트된 데이터를 반환합니다.
+            // false일 때는 업데이트되기 전의 데이터를 반환합니다.
         }).exec();
         if(!post) {
             ctx.status = 404;
